f09693561c937334aa5e00717c168f0142f88899
dumpy <112026806+thatcppnerd@users.noreply.github.com>
2024-04-24 11:57:06 -0500

    pci wipp

--- include/BMGL_pci.h
+++ include/BMGL_pci.h
@@ -1,14 +1,19 @@
 #ifndef __BMGL_PCI_H__
 #define __BMGL_PCI_H__
 
-struct PCI_Architecture
+struct PCI_Arch
 {
     unsigned int buses;
     unsigned int slots;
     unsigned int devices;
 };
 
+struct PCI_DeviceInfo
+{
+    unsigned 
+    unsigned short vendor;
+}
 
 struct PCI_Architecture BMGL_PCI_GetArchitecture(void);
-
+struct PCI_DeviceInfo   BMGL_PCI_GetDeviceInfo();
 #endif
