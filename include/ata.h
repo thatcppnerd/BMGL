2a1e67dd8011dad5841c2ed6135d9fdc4cb1365f
dumpy <112026806+thatcppnerd@users.noreply.github.com>
2024-05-19 22:04:56 -0500

    wip

--- include/ata.h
+++ include/ata.h
@@ -1,6 +1,11 @@
 #ifndef __ATA_H__
 #define __ATA_H__
 
+typedef u32_t LBA_t;
+
+int BMGL_ATA_ReadSectorsPIO(LBA_t lba, int sectors, void* dest);
+int BMGL_ATA_WriteSectorsPIO(LBA_t lba, int s0ectors, void* src);
+
 
 
 #endif
